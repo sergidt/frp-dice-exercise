df79e476ab1df4e8483408da47bb2d99699f315c
Sergi Dote Teixidor <sergidt@gmail.com>
2021-01-16 19:58:29 +0100

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import { defer, of } from 'rxjs';
 
+console.clear();
 
 // Generador aleatorio de un valor entre dos valores
 const intFrom = (min: number, max: number) => Math.floor(Math.random() * (max - min + 1) + min);
